81db2e060a25cd7759bb6fd4832b13d571ac2391
Javan Cook <javan822@hotmail.com>
2015-01-16 12:02:04 +0000

    Added GPIO for everything except analogue sticks

--- ControllerServer.py
+++ ControllerServer.py
@@ -1,7 +1,32 @@
-#Import socket and time
+#Import socket, itertools, GPIO and time
 import socket
 import itertools
+import RPi.GPIO as GPIO
 from time import gmtime, strftime
+
+#Setup GPIO and control values
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+lsb = 7
+rsb = 12
+xbb = 11
+bl = 13
+Ab = 35
+Bb = 36
+Xb = 37
+Yb = 38
+du = 7
+dd = 12
+dl = 11
+dr = 13
+st = 35
+ba = 36
+las = 37
+ras = 38
+rtr = 36
+ltr = 11
+buttonset1 = (ras, las, ba, st, dr, dl, dd, du)
+buttonset2 = (Yb, Xb, Bb, Ab, bl, xbb, rsb, lsb)
 
 #Create control binaries
 binaries = list(itertools.product(range(2), repeat = 8))
@@ -32,23 +57,35 @@ while True:
    conn.send(data)
    #Translate into callable list
    decoded = data.decode()
-   print(decoded)
    listed = data.split()
-   #for x in range(1,257):
+   #for x in range(0,256):
     #Start, back, d-pad and analogue clicks
     #if listed[0].decode() == str(x):
-     #print(binaries[x])
+     #GPIO.setup(buttonset1, GPIO.OUT)
+     #GPIO.output(buttonset1, binaries[x])
     #Xbox button, shoulder buttons, A, B, X and Y
     #if listed[1].decode() == str(x):
-     #print(binaries[x])
+     #GPIO.setup(buttonset2, GPIO.OUT)
+     #GPIO.output(buttonset2, binaries[x])
    #Left trigger
-   #if int(listed[2].decode()) >= 150:
-    #print('left trigger') 
+   if int(listed[2].decode()) >= 150:
+    GPIO.setup(ltr, GPIO.OUT)
+    GPIO.output(ltr, 1)
+   else:
+    GPIO.setup(ltr, GPIO.OUT)
+    GPIO.output(ltr, 0)
    #Right trigger
-   #if int(listed[3].decode()) >= 150:
-    #print('right trigger')
+   if int(listed[3].decode()) >= 150:
+    GPIO.setup(rtr, GPIO.OUT)
+    GPIO.output(rtr, 1)
+   else:
+    GPIO.setup(rtr, GPIO.OUT)
+    GPIO.output(rtr, 0)
+   #Left analogue stick
+   #Right analogue stick
   #Handles shutdown by peer error
   except socket.error as e:
    if e.errno == 104:
+    GPIO.cleanup()
     print(addr , 'disconnected', strftime("%a, %d, %b %Y %H:%M:%S", gmtime()))
     break
