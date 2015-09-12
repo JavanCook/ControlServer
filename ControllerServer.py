05d244e6760a3c635b20a427ececad2c9810bf42
Javan Cook <javan822@hotmail.com>
2015-09-12 08:47:36 +0000

    updated analogue sticks

--- ControllerServer.py
+++ ControllerServer.py
@@ -10,40 +10,41 @@ binaries1 = list(itertools.product(range(2), repeat = 8))
 #Setup GPIO and control values
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-lsb = 7
-rsb = 11
-xbb = 12
-bl = 13
-Ab = 35
-Bb = 36
-Xb = 37
-Yb = 38
-du = 7
-dd = 11
-dl = 12
-dr = 13
-st = 35
-ba = 36
-las = 37
-ras = 38
-rtr1 = 35
-rtr2 = 36
-ltr1 = 11
-ltr2 = 12
-lasl = 7
-lasr = 11
-lasu = 12
-lasd = 13
-rasl = 35
-rasr = 36
-rasu = 37
-rasd = 38
+lsb = 33
+rsb = 8
+xbb = 10
+bl = 31
+Ab = 15
+Bb = 13
+Xb = 3
+Yb = 7
+du = 16
+dd = 22
+dl = 18
+dr = 19
+st = 5
+ba = 12
+las = 23
+ras = 21
+#rtr1 = 35
+#rtr2 = 36
+#ltr1 = 11
+#ltr2 = 12
+lasl = 37
+lasr = 35
+lasu = 38
+lasd = 40
+lasn = 36
+#rasl = 35
+#rasr = 36
+#rasu = 37
+#rasd = 38
 buttonset1 = (ras, las, ba, st, dr, dl, dd, du)
 buttonset2 = (Yb, Xb, Bb, Ab, bl, xbb, rsb, lsb)
-lefttrigger = (ltr1, ltr2)
-righttrigger = (rtr1, rtr2)
-leftanalogue = (lasl, lasr, lasu, lasd)
-rightanalogue = (rasl, rasr, rasu, rasd)
+#lefttrigger = (ltr1, ltr2)
+#righttrigger = (rtr1, rtr2)
+leftanalogue = (lasl, lasr, lasu, lasd, lasn)
+#rightanalogue = (rasl, rasr, rasu, rasd)
 
 #Allows multiple connects/disconnects
 while True:
@@ -73,15 +74,15 @@ while True:
    decoded = data.decode()
    listed = data.split()
    if len(listed) == 8:
-    #for x in range(0,256):
+    for x in range(0,256):
      #Start, back, d-pad and analogue clicks
-     #if listed[0].decode() == str(x):
-      #GPIO.setup(buttonset1, GPIO.OUT)
-      #GPIO.output(buttonset1, binaries[255-x])
+     if listed[0].decode() == str(x):
+      GPIO.setup(buttonset1, GPIO.OUT)
+      GPIO.output(buttonset1, binaries1[255-x])
      #Xbox button, shoulder buttons, A, B, X and Y
-     #if listed[1].decode() == str(x):
-      #GPIO.setup(buttonset2, GPIO.OUT)
-      #GPIO.output(buttonset2, binaries[255-x])
+     if listed[1].decode() == str(x):
+      GPIO.setup(buttonset2, GPIO.OUT)
+      GPIO.output(buttonset2, binaries1[255-x])
     #Left trigger
     #GPIO.setup(lefttrigger, GPIO.OUT)
     #GPIO.output(lefttrigger, 1)
@@ -92,28 +93,31 @@ while True:
     #GPIO.output(righttrigger, 1)
     #if int(listed[3].decode()) >= 150:
      #GPIO.output(righttrigger, 0)
+    #DP setup
     #Left analogue stick
     GPIO.setup(leftanalogue, GPIO.OUT)
-    GPIO.output(leftanalogue, 1)
+    #GPIO.output(lasn, 1)
     if int(listed[4].decode()) >= 65000:
-     GPIO.output((lasl, lasr), (0,1))
-    if int(listed[4].decode()) <= 500:
      GPIO.output((lasl, lasr), (1,0))
+    if int(listed[4].decode()) <= 500:
+     GPIO.output((lasl, lasr), (0,1))
+    if 10000< int(listed[5].decode()) > 50000:
+     GPIO.output((lasu, lasd, lasn), (0,0,1))
     if int(listed[5].decode()) >= 65000:
-     GPIO.output((lasu, lasd), (0,1))
+     GPIO.output((lasu, lasd, lasn), (1,0,0))
     if int(listed[5].decode()) <= 500:
-     GPIO.output((lasu, lasd), (1,0))
+     GPIO.output((lasu, lasd, lasn), (0,1,0))
     #Right analogue stick
-    GPIO.setup(rightanalogue, GPIO.OUT)
-    GPIO.output(rightanalogue, 1)
-    if int(listed[6].decode()) >= 65000:
-     GPIO.output((rasl, rasr), (0,1))
-    if int(listed[6].decode()) <= 500:
-     GPIO.output((rasl, rasr), (1,0))
-    if int(listed[7].decode()) >= 65000:
-     GPIO.output((rasu, rasd), (0,1))
-    if int(listed[7].decode()) <= 500:
-     GPIO.output((rasu, rasd), (1,0))
+    #GPIO.setup(rightanalogue, GPIO.OUT)
+    #GPIO.output(rightanalogue, 1)
+    #if int(listed[6].decode()) >= 65000:
+     #GPIO.output((rasl, rasr), (0,1))
+    #if int(listed[6].decode()) <= 500:
+     #GPIO.output((rasl, rasr), (1,0))
+    #if int(listed[7].decode()) >= 65000:
+     #GPIO.output((rasu, rasd), (0,1))
+    #if int(listed[7].decode()) <= 500:
+     #GPIO.output((rasu, rasd), (1,0))
   #Handles disconnect by peer error
   except socket.error as e:
    if e.errno == 104:
